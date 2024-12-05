3f8316391c341657806d0d0dc6705724b1a885c3
Garik <khvatik.igor@gmail.com>
2024-12-06 00:57:40 +0300

    fix: filename time to date

--- tomato/core/logger_settings.py
+++ tomato/core/logger_settings.py
@@ -3,7 +3,7 @@ from loguru import logger
 
 def logger_setup() -> None:
     logger.add(
-        "logs/log_{time}.log",
+        "logs/log_{time:%Y-%m-%d}.log",
         rotation="23:55",
         retention="10 days",
     )
