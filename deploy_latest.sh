698441d44adce8f6f41a358f7d73dd136eefc7b6
Garik <khvatik.igor@gmail.com>
2024-12-13 01:12:48 +0300

    add deploy_latest.sh and deploy_stable.sh

---
+++ deploy_latest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Остановка текущих контейнеров
+docker-compose down
+
+# Переключение на ветку latest
+git checkout latest
+git pull origin latest
+
+# Запуск docker-compose
+docker-compose up -d
---
+++ deploy_stable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Остановка текущих контейнеров
+docker-compose down
+
+# Переключение на ветку stable
+git checkout stable
+git pull origin stable
+
+# Запуск docker-compose
+docker-compose up -d
