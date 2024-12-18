2e290744cfddc29c72169a60557d085239e2e7d8
Garik <khvatik.igor@gmail.com>
2024-12-18 11:52:34 +0300

    feat: add set_waiting_time with for_self_delivery=True to set_default_time func

--- set_default_time.py
+++ set_default_time.py
@@ -11,6 +11,7 @@ from tomato.core.settings import SETTINGS
 async def set_default_time() -> None:
     """Устанавливает время ожидания по умолчанию для всех организаций."""
     logger.info('Установка дефолтного времени...')
+    
     try:
         token = get_tomato_auth_token()
     except Exception as e:
@@ -21,21 +22,32 @@ async def set_default_time() -> None:
         return
 
     try:
-        # Получаем дефолтное время для зоны Азов во всех организациях(сейчас оно одинаковое)
-        default_time = SETTINGS.DEFAULT_ZONES_TIMES["AZOV_ALL_ORGANIZATIONS"]
+        # Получаем дефолтное время для доставки в зонах Азов во всех организациях(сейчас оно одинаковое)
+        default_delivery_time = SETTINGS.DEFAULT_ZONES_TIMES["AZOV_ALL_ORGANIZATIONS"]
+        # Аналогично время самовывоза
+        default_self_delivery_time = SETTINGS.DEFAULT_ZONES_TIMES["SELF-DELIVERY_ALL_ORGANIZATIONS"]
+
         # Перебираем id тредов и id организаций
         for thread_id, organization_id in SETTINGS.CHAT_ID_TO_ORGANIZATION_ID.items():
-            # Устанавливаем дефолтное время
-            set_waiting_time(organization_id, default_time, token)
+            # Устанавливаем дефолтное время для зон доставки
+            set_waiting_time(organization_id, default_delivery_time, token)
+            # Устанавливаем дефолтное время для самовывоза
+            set_waiting_time(
+                organization_id, default_self_delivery_time, token, for_self_delivery=True
+            )
+            # Поучаем строку с информацией о текущем времени ожидания во всех зонах включая самовывоз
+            time_string = get_current_waiting_time_string(organization_id, token, for_all_zones=True)
 
             await bot.send_message(
                 chat_id=SETTINGS.MAIN_CHAT_ID, text=f'Дефолтное время установлено', message_thread_id=thread_id
             )
-            time_string = get_current_waiting_time_string(organization_id, token)
+
             await bot.send_message(
                 chat_id=SETTINGS.MAIN_CHAT_ID, text=time_string, message_thread_id=thread_id
             )
+
             logger.info("Дефолтное время установлено\n")
+
     except Exception as e:
         err = f'Ошибка при установке дефолтного времени: {e}'
         logger.exception(err)
