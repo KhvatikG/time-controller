fe9c0065e21eeff63967135158646d05692ba9b6
Garik <khvatik.igor@gmail.com>
2025-03-27 03:06:13 +0300

    fix: add logging to get_report_from

--- reports.py
+++ reports.py
@@ -5,7 +5,7 @@ from bot_init import bot
 from db.models.order_closer_chat import OrderCloserChat
 from tomato.core.api.auth import get_tomato_auth_token
 from tomato.report import get_order_report_by_departments
-
+from loguru import logger
 
 async def send_departments_report(date: str = None, chats: list[OrderCloserChat] = None) -> None:
     """Отправляет дневной в чат
@@ -13,9 +13,14 @@ async def send_departments_report(date: str = None, chats: list[OrderCloserChat]
     :param chats: Чаты для отправки отчета
     :param date: дата в формате YYYY-MM-DD по умолчанию None - будет использована сегодняшняя дата
     """
+    logger.info("Отправка дневного отчета по отделам")
+    logger.debug("Получение токена")
     token = get_tomato_auth_token()
 
+    logger.debug("Получение дневного отчета")
     df = get_order_report_by_departments(token=token)
+    logger.debug(f"Отчет получен\n{df}")
+    logger.info("Начинаю построение сообщений...")
     for i, row in df.iterrows():
         department = row.get('Ресторан')
         count_orders = row.get('Количество заказов')
@@ -31,5 +36,7 @@ async def send_departments_report(date: str = None, chats: list[OrderCloserChat]
             f"  Сумма доставки для клиента: {italic(delivery_sum)}\n"
             f"  Сумма по всем заказам: {bold(summ)}\n"
         )
+        logger.info(f"Сообщение построено\n{message}")
+        logger.info("Отправка сообщения")
         for chat in chats:
             await bot.send_message(chat.id, message, parse_mode=ParseMode.MARKDOWN)
